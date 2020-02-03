a8ca406b23a665037fe63af52b810a09f4927ded
cplepel-py <60494947+cplepel-py@users.noreply.github.com>
2020-02-03 12:38:51 -0600

    Add 3-encoder forward method
    Added a method to move the robot forward using input from 3 encoders (on the front two wheels and the back left wheel).
    This functionality was added to the forward_enc() method, taking the same tiles argument as the previous forward() method. The forward() method now delegates to this new method, but the time-base movement method still exists with the name forward_time().
    This commit is a proof of concept before other motions are edited to be controlled by encoder readings.

--- AutoOp.java
+++ AutoOp.java
@@ -41,6 +41,8 @@ public abstract class AutoOp extends LinearOpMode {
     private double armStatic = 0.02;
     protected double wheelPower = 0.75;
     private double margin = 1;
+    private double ticksPerTile = 1120/4/3.141592653589793238462643383279502884*24; // 1120 ticks/rev * 1/4pi rev/in * 24 in/tile
+    private double strafeTraction = 0.9;
     private double turningCorrection = 0.001;
     
     public void initialize(HardwareMap hardwareMap, Telemetry telemetry, boolean has_arm, boolean has_claw){
@@ -170,7 +172,7 @@ public abstract class AutoOp extends LinearOpMode {
         stopWheels();
     }
     
-    public void forward(double tiles){
+    public void forward_time(double tiles){
         leftWheels(wheelPower);
         rightWheels(wheelPower);
         runtime.reset();
@@ -179,6 +181,30 @@ public abstract class AutoOp extends LinearOpMode {
             telemetry_.update();
         }
         stopWheels();
+    }
+    public void forward_enc(double tiles){
+        leftfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftback.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightfront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftfront.setTargetPosition(tiles*ticksPerTile);
+        leftback.setTargetPosition(tiles*ticksPerTile);
+        rightfront.setTargetPosition(tiles*ticksPerTile);
+        leftfront.setPower(wheelPower);
+        leftback.setPower(wheelPower);
+        rightfront.setPower(wheelPower);
+        rightback.setPower(wheelPower);
+        while(opModeIsActive() && leftback.isBusy()){  // If the front two wheels are still running, the robot will continue straight
+            telemetry_.addData("Path", "Forward: %s lf, %s rf, %s lb", leftfront.getCurrentPosition(), rightfront.getCurrentPosition(), leftback.getCurrentPosition());
+            telemetry_.update();
+        }
+        rightback.setPower(0.0);
+        while(opModeIsActive() && (leftfront.isBusy() || rightfront.isBusy())){
+            telemetry_.addData("Path", "Forward: %s lf, %s rf, %s lb", leftfront.getCurrentPosition(), rightfront.getCurrentPosition(), leftback.getCurrentPosition());
+            telemetry_.update();
+        }
+    }
+    public void forward(double tiles){
+        forward_enc(tiles);
     }
     public void backward(double tiles){
         leftWheels(-wheelPower);
