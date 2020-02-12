692a9f62a4642a018cdc702f1eff63e759aa3939
cplepel-py <60494947+cplepel-py@users.noreply.github.com>
2020-02-12 15:18:42 -0600

    Finish draft of Vuforia back-end
    Finish implementation of Vuforia initialization, add a skystone detection method, and add a skysktone seeking method to automatically move to the nearest skystone.

--- VuforiaOp.java
+++ VuforiaOp.java
@@ -1,6 +1,7 @@
 package workspace;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
@@ -24,8 +25,12 @@ public abstract class VuforiaOp extends AutoOp {
     
     protected OpenGLMatrix lastKnownLocation;
     protected OpenGLMatrix phoneLocation;
+    public double red_off;  // x; positive when skystone is right of the phone
+    public double blue_off;  // y; negative if skystone in viewig field
+    public double green_off;  // z; skystones should start on the ground.
+    public float[] raw_offset;  // green, red, blue; zxy
     
-    protected static final String VUFORIA_KEY = "";
+    protected static final String VUFORIA_KEY = "AeTCFCP/////AAABmfHb70GlwET4rTC9SGWFDMVfC6cZN+OykKIBJrcpYIVTGzbWZ11w8AoTq6mrdM68JbbKzA3C/+v46Jo8+pcraqzQ5QPOv23oHfxgDMKAlbxYGixALMMcSsE8Anv2NeBhMd+zLoRTYAnU8YQ7S35RYmeFRufFLUHF+Tkps1tZoWOxd6yyzuw10nwVVlZ1F4JhHVEHXoFsvUDXVxHENaFOxcy244tY+AM/qM4U9jD1RXLMPrp+ZLWmG/Bt8ja1avZHE0EWK5DKaKD0uWrT9A9WwT8w+aNjNxJKOdL9mq/fhZwDRPS6Q7flBwIQvVbq6bTXl4erMprFkPXo4yaZJvJo8ag+V5mEVe/cQ2N0A7qNtb3f";
     
     public void initVision(HardwareMap hardwareMap){
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -36,11 +41,18 @@ public abstract class VuforiaOp extends AutoOp {
         visionTargets = localizer.loadTrackablesFromAsset("Skystone");
         target = visionTargets.get(0);
         target.setName("skystone");
-        target.setLocation(createMatrix(0, 0, 0, 0, 0, 0));
-        phoneLocation = createMatrix(0, 0, 0, 0, 0, 0);
+        //target.setLocation(createMatrix(0, 0, 0, 0, 0, 0));
+        //phoneLocation = createMatrix(0, 0, 0, 0, 0, 0);
         listener = (VuforiaTrackableDefaultListener) target.getListener();
-        listener.setPhoneInformation(phoneLocation, params.cameraDirection);
-        lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
+        //listener.setPhoneInformation(phoneLocation, params.cameraDirection);
+        //lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
+        green_off = 0;
+        red_off = 0;
+        blue_off = 0;
+        raw_offset = null;
+    }
+    
+    public void activateVision(){
         visionTargets.activate();
     }
     
@@ -50,9 +62,34 @@ public abstract class VuforiaOp extends AutoOp {
                 AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
     }
     
-    public float[] getOffset(){
-        OpenGLMatrix latestLocation = null;
-        float[] r = {0, 0};
-        return r;
+    public void calcOffset(){
+        OpenGLMatrix pose = listener.getPose();
+        if(pose == null){
+            raw_offset = null;
+            return;
+        }
+        VectorF trans = pose.getTranslation();
+        float[] off = {trans.get(0), trans.get(1), trans.get(2)};
+        // smooth the readings
+        green_off = 0.9*green_off + 0.1*off[0];
+        red_off = 0.9*red_off + 0.1*off[1];
+        blue_off = 0.9*blue_off + 0.1*off[2];
+        raw_offset = off;
+    }
+    
+    public void toSkystone(double threshold, double nullspd, double factor){
+        while(opModeIsActive() && (raw_offset == null || red_off < -threshold || red_off > threshold)){
+            telemetry.addData("Path", "seeking skystone");
+            calcOffset();
+            if(raw_offset == null){
+                strafeRightSpd(nullspd);
+                telemetry.addData("Skystone", "missing");
+            }
+            else{
+                strafeLeftSpd(red_off/factor);
+                telemetry.addData("Skystone", "%.0f", red_off);
+            }
+            telemetry.update();
+        }
     }
 }
