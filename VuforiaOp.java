b44233b47157e4179ede875ec65729b6d07fdba3
cplepel-py <60494947+cplepel-py@users.noreply.github.com>
2020-02-13 19:02:25 -0600

    Add Camera Position Corrections
    Add `setCameraPosition()` to set the position of the camera. This is stored in the private variables `cam_x`, `cam_y`, and `cam_z`. All measurements are now in mm and from the "reference" on the robot (the camera by default). Also, add an additional patch to fix the NullPointerException when recognizing objects with a webcam.

--- VuforiaOp.java
+++ VuforiaOp.java
@@ -31,13 +31,15 @@ public abstract class VuforiaOp extends AutoOp {
     public double green_off;  // z; skystones should start on the ground.
     public float[] raw_offset;  // green, red, blue; zxy
     
+    private double cam_x, cam_y, cam_z;
+    
     protected static final String VUFORIA_KEY = "AeTCFCP/////AAABmfHb70GlwET4rTC9SGWFDMVfC6cZN+OykKIBJrcpYIVTGzbWZ11w8AoTq6mrdM68JbbKzA3C/+v46Jo8+pcraqzQ5QPOv23oHfxgDMKAlbxYGixALMMcSsE8Anv2NeBhMd+zLoRTYAnU8YQ7S35RYmeFRufFLUHF+Tkps1tZoWOxd6yyzuw10nwVVlZ1F4JhHVEHXoFsvUDXVxHENaFOxcy244tY+AM/qM4U9jD1RXLMPrp+ZLWmG/Bt8ja1avZHE0EWK5DKaKD0uWrT9A9WwT8w+aNjNxJKOdL9mq/fhZwDRPS6Q7flBwIQvVbq6bTXl4erMprFkPXo4yaZJvJo8ag+V5mEVe/cQ2N0A7qNtb3f";
     
     public void initVision(HardwareMap hardwareMap){
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         params = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         params.vuforiaLicenseKey = VUFORIA_KEY;
-        //params.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        params.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         localizer = ClassFactory.createVuforiaLocalizer(params);
         visionTargets = localizer.loadTrackablesFromAsset("Skystone");
@@ -52,35 +54,36 @@ public abstract class VuforiaOp extends AutoOp {
         red_off = 0;
         blue_off = 0;
         raw_offset = null;
+        setPhonePosition(0, 0, 0);
     }
     
     public void activateVision(){
         visionTargets.activate();
     }
     
-    /*public OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w){
+    private OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w){
         return OpenGLMatrix.translation(x, y, z)
             .multiplied(Orientation.getRotationMatrix(
                 AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
-    }*/
+    }
     
     public void calcOffset(){
-        OpenGLMatrix pose = listener.getPose();
+        OpenGLMatrix pose = null;
+        pose = listener.getFtcCameraFromTarget();//getPose();
         if(pose == null){
             raw_offset = null;
             return;
         }
         VectorF trans = pose.getTranslation();
-        float[] off = {trans.get(0), trans.get(1), trans.get(2)};
+        float[] off = /*{0,0,0};*/{trans.get(0), trans.get(1), trans.get(2)};
         // smooth the readings
-        green_off = 0.9*green_off + 0.1*off[0];
-        red_off = 0.9*red_off + 0.1*off[1];
-        blue_off = 0.9*blue_off + 0.1*off[2];
+        green_off = 0.9*green_off + 0.1*(off[0]+cam_z);
+        red_off = 0.9*red_off + 0.1*(off[1]-cam_x);
+        blue_off = 0.9*blue_off + 0.1*(off[2]-cam_y);
         raw_offset = off;
     }
     
     public void toSkystone(double threshold, double nullspd, double factor){
-        resetWheels();
         while(opModeIsActive() && (raw_offset == null || red_off < -threshold || red_off > threshold)){
             telemetry.addData("Path", "seeking skystone");
             calcOffset();
@@ -94,5 +97,28 @@ public abstract class VuforiaOp extends AutoOp {
             }
             telemetry.update();
         }
+    }
+    public void toSkystone(double threshold, double nullspd){
+        toSkystone(threshold, nullspd, 1000.0);
+    }
+    public void toSkystone(double threshold){
+        toSkystone(threshold, 0.1);
+    }
+    public void toSkystone(){
+        toSkystone(10.0);
+    }
+    
+    public void setCameraPosition(double horiz, double depth, double height){
+        // The following refers to "reference". This should probably be a point
+        // on the stationary plate of the claw.
+        // horiz: mm right of the reference.
+        // depth: mm behind the reference.
+        // height: mm above the reference.
+        cam_x = horiz;
+        cam_y = depth;
+        cam_z = height;
+        red_off -= cam_x;
+        blue_off -= cam_y;
+        green_off += cam_z;
     }
 }
